b4fe63da0be18c47876ef1c32f288b78f2e8888b
experimentalarchives <44888344+experimentalarchives@users.noreply.github.com>
2018-11-14 16:48:36 -0600

    Update generate-medium.js

--- mtp/generate-medium.js
+++ mtp/generate-medium.js
@@ -39,7 +39,7 @@ function generate() {
     document.getElementById("code1").style.display = "block";
     document.getElementById("code2").style.display = "block";
     document.getElementById("desc").style.textAlign = "left";
-    document.getElementById("desc").innerHTML = "<b>First code:</b> Paste it where you want the player to be. Change the margin numbers for exact positioning.<br><b>Second code:</b> Paste it just before /body (located near the end of your blog or website’s HTML)."
+    document.getElementById("desc").innerHTML = "<b>First code:</b> Paste it where you want the player to be. For exact positioning, change the margin-top and margin-left numbers.<br><b>Second code:</b> Paste it just before /body (located near the end of your blog or website’s HTML)."
     
     // declare variables for multi-info and multi-names spans
 
