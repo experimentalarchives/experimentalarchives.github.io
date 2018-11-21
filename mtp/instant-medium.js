61db03c672d6ed8af0e232be659374dd8b43bb56
experimentalarchives <44888344+experimentalarchives@users.noreply.github.com>
2018-11-21 12:55:52 -0600

    Update instant-medium.js

--- mtp/instant-medium.js
+++ mtp/instant-medium.js
@@ -32,7 +32,7 @@ function mtp() {
 
     // style player elements
 
-    document.getElementById("yukoki_mplayer”);
+    document.getElementById("yukoki_mplayer");
         document.getElementById("yukoki_mplayer").style.width = "170px";
         document.getElementById("yukoki_mplayer").style.height = "20px";
         document.getElementById("yukoki_mplayer").style.overflow = "hidden";
