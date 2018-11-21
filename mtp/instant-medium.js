bb0490d23d5e1763e4b339cf36c4286a04323347
experimentalarchives <44888344+experimentalarchives@users.noreply.github.com>
2018-11-21 12:16:53 -0600

    Update instant-medium.js

--- mtp/instant-medium.js
+++ mtp/instant-medium.js
@@ -40,8 +40,8 @@ function mtp() {
     var  mplgif = document.getElementById("mplgif");
         mplgif.style.margin = "0 0 0 10px;";
     var ongaku = document.getElementById("ongaku");
-    ongaku.onmouseover = function() { this.style.margin = "3px 0 0 12px"; };
-    ongaku.onmouseout = function() { this.style.margin = "-20px 0 0 12px"; };
+    ongaku.onmouseover = function() { this.style.margin = "3px 0px 0px 12px"; };
+    ongaku.onmouseout = function() { this.style.margin = "-20px 0px 0px 12px"; };
     ongaku.style.margin = "-20px 0 0 12px";
     ongaku.style.transition = "all 0.6s";
 
