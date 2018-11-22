7345fbde24d3864786db4437f2749478f621f5fd
experimentalarchives <44888344+experimentalarchives@users.noreply.github.com>
2018-11-21 22:59:05 -0600

    Update multitune-mini.js

--- mtp/multitune-mini.js
+++ mtp/multitune-mini.js
@@ -63,6 +63,10 @@ window.onload = function mtp() {
     document.getElementById("mtplayer").style.width = "25px";
     document.getElementById("mtplayer").style.padding = "0px 15px";
 
+    document.getElementById("mtplayer").style.display = "block";
+    document.getElementById("mtplayer").style.width = "20px";
+    document.getElementById("mtplayer").style.padding = "3px 5px";
+
     var xaudio = document.getElementById("xaudio");
     xaudio.style.display = "none";
 
